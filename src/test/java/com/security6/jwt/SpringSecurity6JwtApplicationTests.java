3df786d7597d2143ca8143bea31f663e5a6c3e77
mamish12 <mku20sahu@gmail.com>
2024-01-05 11:09:33 +0530

    spring security6

---
+++ src/test/java/com/security6/jwt/SpringSecurity6JwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.security6.jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurity6JwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
